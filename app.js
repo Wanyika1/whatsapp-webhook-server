5fd4a255512209e3149e8e70afb5a0c0a63a434f
Wanyika1 <166023596+Wanyika1@users.noreply.github.com>
2025-12-27 23:35:06 +0300

    Initialize Express webhook server

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+const VERIFY_TOKEN = "verify_token";
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post("/webhook", (req, res) => {
+  console.log(JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.get("/", (req, res) => {
+  res.send("Webhook server running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Running on", PORT));
